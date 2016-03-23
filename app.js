0efd491fd59cfbbb5b9f4e0713409a7d4a932ff5
sagar.sutaria@pb.com <Seq@u2e96>
2016-03-23 19:10:28 +0530

    Corrected ui.router DI, which was mispelled to ui-router

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-(function() {
-	var app = angular.module("flapperNews", ["ui-router"]);
+//(function() {
+	var app = angular.module("flapperNews", ["ui.router"]);
 
 	app.config([
 		'$stateProvider',
@@ -59,4 +59,4 @@
 			};
 		}
 	]);
-})();
+//})();
